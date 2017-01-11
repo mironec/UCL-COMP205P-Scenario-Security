560d9677b75f93ca662abd37abac0c03a3b8671e
Miron Zelina <mironzelinajr@gmail.com>
2017-01-11 17:27:23 +0000

    Add browsing others' profile pages
    Browse profiles with profile.php?user=insertsomeusernamehere.

--- Vulnerable site/includes/templates/profile.php
+++ Vulnerable site/includes/templates/profile.php
@@ -1,8 +1,15 @@
-Welcome, <?php echo $user->getUsername(); ?>!<br />
+<?php if($user == NULL){ ?>
+Welcome, <?php echo $currentuser->getUsername(); ?>!<br />
 <br />
 Change your profile:<br />
  <form action="?" method="POST">
-  Bio: <textarea name="bio" style="vertical-align: top; width = 300px; height = 50px;"><?php echo $user->getBio(); ?></textarea><br />
-  Email: <input name="email" type="text" value="<?php echo $user->getEmail(); ?>" /><br />
+  Bio: <textarea name="bio" style="vertical-align: top; width = 300px; height = 50px;"><?php echo $currentuser->getBio(); ?></textarea><br />
+  Email: <input name="email" type="text" value="<?php echo $currentuser->getEmail(); ?>" /><br />
   <input value="Update" type="submit">
  </form>
+<?php } else { ?>
+Welcome to the profile for <?php echo $user->getUsername(); ?>!<br />
+<br />
+Email: <?php echo $user->getEmail(); ?><br />
+Bio: <?php echo $user->getBio(); ?>
+<?php } ?>
--- Vulnerable site/profile.php
+++ Vulnerable site/profile.php
@@ -3,7 +3,11 @@ require_once("includes/databaseStart.php");
 require_once('includes/requireAuthenticated.php');
 require_once('includes/models/user.php');
 
-$user = User::getUserByID($_SESSION['userid']);
+$user = NULL;
+if(isset($_GET['user']) && !empty($_GET['user'])){
+	$user = User::getByUsername($_GET['user']);
+}
+$currentuser = User::getUserByID($_SESSION['userid']);
 
 if(isset($_POST['bio']) && isset($_POST['email'])){
 	$user->setBio($_POST['bio']);
